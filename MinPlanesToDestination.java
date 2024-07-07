b5e38d21519a94d00ebf9b45bd583f82db2f7b90
Ujjurath <43915154+Ujjurath@users.noreply.github.com>
2024-07-07 19:03:52 +0530

    Update MinPlanesToDestination.java

--- MinPlanesToDestination.java
+++ MinPlanesToDestination.java
@@ -1,1 +1,37 @@
+public class MinPlanesToDestination {
+    public static int minPlanesToDestination(int[] fuel) {
+        int n = fuel.length;
+        if (n == 1) return 0;
 
+        int jumps = 0;
+        int currentEnd = 0;
+        int farthest = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (i > farthest) {
+                return -1;
+            }
+
+            farthest = Math.max(farthest, i + fuel[i]);
+
+            if (i == currentEnd) {
+                jumps++;
+                currentEnd = farthest;
+
+                if (currentEnd >= n - 1) {
+                    return jumps;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] fuel1 = {2, 1, 2, 3, 1};
+        int[] fuel2 = {1, 6, 3, 4, 5, 0, 0, 0, 6};
+
+        System.out.println(minPlanesToDestination(fuel1)); // Output: 2
+        System.out.println(minPlanesToDestination(fuel2)); // Output: 3
+    }
+}
